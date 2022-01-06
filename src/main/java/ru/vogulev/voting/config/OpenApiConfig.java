eb19866c694d54c1907be074ec5aefc28a7f0ae6
Artem.Vogulev <Artem.Vogulev@russianpost.ru>
2022-01-06 12:01:58 +0300

    changed Swagger description

--- src/main/java/ru/vogulev/voting/config/OpenApiConfig.java
+++ src/main/java/ru/vogulev/voting/config/OpenApiConfig.java
@@ -21,8 +21,8 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(
                 title = "REST API documentation",
                 version = "1.0",
-                description = "Приложение по <a href='https://javaops.ru/view/topjava2'>курсу TopJava-2</a> (решение выпускного проекта)",
-                contact = @Contact(url = "https://javaops.ru/#contacts", name = "Grigory Kislin", email = "admin@javaops.ru")
+                description = "Voting system for deciding where to have lunch(Graduation project for education program TopJava)",
+                contact = @Contact(url = "https://www.linkedin.com/in/artem-vogulev", name = "Artem Vogulev", email = "art.vogulev@gmail.com")
         ),
         security = @SecurityRequirement(name = "basicAuth")
 )
